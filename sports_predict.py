1bfa20960f6b8ff7e50655ebcc4520a894b31987
Neba Roland <53044894+Roland-coder@users.noreply.github.com>
2021-10-30 03:54:14 +0200

    Update sports_predict.py

--- sports_predict.py
+++ sports_predict.py
@@ -56,6 +56,7 @@ if st.button('Predict Overall Performance'):
 # 	prediction = model.predict(features)
 	st.header("Please find predicted value below")
 # 	st.write("The overall predicted score for the above player is", np.round(prediction[0]))
-  	st.write("The overall predicted score for the above player is", aa)
+  	
+	st.write("The overall predicted score for the above player is", np.round(prediction[0]))
 else:
 	st.write('Thank You For Trusting Us')
